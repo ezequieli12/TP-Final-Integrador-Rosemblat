b0b1f1786874dc3de8a4d4d0225ee31cc142918d
unknown <48675678@est.ort.edu.ar>
2025-10-16 19:47:45 -0300

    main.jsx y app.jsx actualizados para establecer conexiones, rutas e imports

--- mi-presupuesto/src/main.jsx
+++ mi-presupuesto/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './index.css'
+import ThemeProvider from './context/ThemeProvider.jsx'
+import MovementsProvider from './context/MovementsProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <MovementsProvider>
+          <App />
+        </MovementsProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
